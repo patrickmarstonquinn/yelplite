66a9afecc9727f556fddb9bfb1a9058425eb6927
Raghu Betina <raghu@firstdraft.com>
2017-01-11 20:47:48 +0000

    Generate reviews resource with starter:resource

---
+++ db/migrate/20170111204748_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :restaurant_id
+      t.string :body
+      t.string :rating
+
+      t.timestamps
+
+    end
+  end
+end
