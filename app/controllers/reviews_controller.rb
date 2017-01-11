6301381093a9a624726ff771d416f2efd51e2992
Raghu Betina <raghu@firstdraft.com>
2017-01-11 20:48:48 +0000

    Automatically associate comment to signed in user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -49,8 +49,6 @@ class ReviewsController < ApplicationController
 
   def update
     @review = Review.find(params[:id])
-
-    @review.user_id = params[:user_id]
     @review.restaurant_id = params[:restaurant_id]
     @review.body = params[:body]
     @review.rating = params[:rating]
